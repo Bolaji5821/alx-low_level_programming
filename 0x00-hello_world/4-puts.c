eea1e9bc56fdd8c4e088777c0079ae43fb0d1fa0
Bolaji5821 <oyedelebolaji84@gmail.com>
2022-03-10 10:20:16 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /*
- * this file contains a main function 
- * that prints a text string and a new line*/
+ * this file contains a main function
+ * that prints a text string and a new line
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
