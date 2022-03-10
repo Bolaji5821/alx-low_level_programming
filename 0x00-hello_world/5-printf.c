669262bec44a2906c0fc24d59c7cb2a4733d98d9
Bolaji5821 <oyedelebolaji84@gmail.com>
2022-03-10 10:49:44 -0800

    puts

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main - this function prints text with printf
+ * return:0 to be true
+ */
+int main (void){
+printf("with proper grammar, but the outcome is a piece of art\n")
+return (0)
+}
