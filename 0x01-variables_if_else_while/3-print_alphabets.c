61fcaa32673079aa9503683447fcfb68d0429e81
Bolaji5821 <oyedelebolaji84@gmail.com>
2022-03-11 06:19:23 -0800

    random

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - prints char a-z, A-Z
+ * it executes, and prints it
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+{
+	printf("%c ", ch);
+}
+for (ch = 'A'; ch <= 'Z'; ch++)
+{
+printf("%c", ch);
+}
+return (0)
+}
