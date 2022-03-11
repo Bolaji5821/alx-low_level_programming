bce900e3d63f7bff9f54432b11742ae6fae33c73
Bolaji5821 <oyedelebolaji84@gmail.com>
2022-03-11 05:40:08 -0800

    random

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,7 +17,7 @@ l = n % 10;
 printf("Last digit of %d is %d", n, l);
 if (l > 5)
 printf(" and is greater than 5\n");
-else if(l == 0) 
+else if (l == 0)
 printf(" and is 0\n");
 else if (l < 6)
 printf(" and is less than 6 and not 0\n");
