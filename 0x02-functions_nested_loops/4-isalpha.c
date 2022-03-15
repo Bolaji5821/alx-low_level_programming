7ba02d41e13c1d562fd14d434d66ce0a00a01d84
Bolaji5821 <oyedelebolaji84@gmail.com>
2022-03-15 14:17:37 -0700

    alpha checker

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,12 +4,10 @@
  *
  * @c will be checked
  *
- * returns: 1 if c is a letter,lowercase or uppercasr
- * returns: 0 otherwise
- */  
+ * returns: 1 if c is a letter,lowercase or uppercase
+ * then otherwise 0
+ */
 int _isalpha(int c)
 {
 return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
-
-
