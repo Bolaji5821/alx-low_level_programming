ad315857cbf0e53d3304c9eb220249239f312fc6
Bolaji5821 <oyedelebolaji84@gmail.com>
2022-03-15 14:09:06 -0700

    alpha checker

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isalpha - this function checks for alphabetic character
+ *
+ * @c will be checked
+ *
+ * returns: 1 if c is a letter,lowercase or uppercasr
+ * returns: 0 otherwise
+ */  
+int _isalpha(int c);
+{
+return (c >= 'a'; && c <= 'z');
+}
+
+
