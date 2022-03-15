a602bfafd77ed11c0aabe53e44c76ba318d92f24
Bolaji5821 <oyedelebolaji84@gmail.com>
2022-03-15 15:33:50 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,7 +11,7 @@ int print_sign(int n)
 {
 if (n > 0)
 {
-	_putchar(+);
+	_putchar(43);
 	return(0);
 }
 else if (n == 0)
@@ -21,7 +21,7 @@ else if (n == 0)
 }
 else
 {
-	_putchar(-);
+	_putchar(45);
 }
-return (-1)
+return (-1);
 }
