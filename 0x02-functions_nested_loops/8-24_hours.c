7e2df04ce37edb75dd0b46b58d3a7cd86e946dc2
Bolaji5821 <oyedelebolaji84@gmail.com>
2022-03-15 19:04:31 -0700

    24hrs

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,14 +16,14 @@ void jack_bauer(void)
 		b = 0;
 		while (b < 60)
 		{
-					_putchar((a / 10) + '0');
-					_putchar((a % 10) + '0');
-					_putchar(':');
-					_putchar((b / 10) + '0');
-					_putchar((b % 10) + '0');
-					_putchar('\n');
-					b++;
-}
-a++;
-}
+			_putchar((a / 10) + '0');
+			_putchar((a % 10) + '0');
+			_putchar(':');
+			_putchar((b / 10) + '0');
+			_putchar((b % 10) + '0');
+			_putchar('\n');
+			b++;
+		}
+		a++;
+	}
 }
