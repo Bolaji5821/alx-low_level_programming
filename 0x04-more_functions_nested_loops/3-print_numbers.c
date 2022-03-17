65c4f077eb6dbed9b28e0676c9ebb56b8e43d734
Bolaji5821 <oyedelebolaji84@gmail.com>
2022-03-17 14:05:45 -0700

    prints 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - prints the number, from 0-9
+ *
+ * Return: no return.
+ */
+void print_numbers(void)
+{
+	int b;
+for (b = 10; b < 20; b++)
+{
+_putchar(b);
+}
+_putchar('\n');
+}
