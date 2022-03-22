ab0cc4cc0b7cbfb5146e54ed6899eac54dd5be43
Bolaji5821 <oyedelebolaji84@gmail.com>
2022-03-21 19:21:47 -0700

    swaps int value

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * swap_int - swaps the values of
+ * *a and *b
+ * @a: a is the first input parameter
+ * @b: b is the second input parameter
+ */
+void swap_int(int *a, int *b)
+{
+        int n;
+
+        n = *b;
+        *b = *a;
+        *a = n;
+}
