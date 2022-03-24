d89361f63a3c77c7c193137eff1b057dc649255f
Bolaji5821 <oyedelebolaji84@gmail.com>
2022-03-24 06:16:57 -0700

    test

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main"
+/**
+ * *_strcat - concatenates two strings.
+ * concatenates two strings and overwriting the terminating null
+ * byte at the end of strings.
+ *
+ *Return: the program returns (dest) after suceefull execution.
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
+	for (dest[i] != '\0'; i++)
+	{}
+	for (src[j] != '\0'; j++)
+{
+	*dest[i] = *src[j]
+		i++;
+}
+	return (dest);
+}
