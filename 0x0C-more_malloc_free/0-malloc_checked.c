8d064b3500cfedf0df47b0ad6e494a6966901cbf
Bolaji5821 <oyedelebolaji84@gmail.com>
2022-04-07 21:44:34 -0700

    malloc task 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -8,14 +8,12 @@
  *
  *  Return: a pointer to the allocated memory.
  */
-void *malloc_checked(unsigned int b)                           
-{                                                              
-        void *ptr;                                             
-                                                               
-        ptr = malloc(b);                                       
-                                                               
-        if (ptr == NULL)                                       
-                exit(98);                                      
-                                                               
-        return (ptr);                                          
-}                        
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b)
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
+}
