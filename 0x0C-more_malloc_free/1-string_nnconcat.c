ccac2948405d7794096d102d2df3504c2763c932
Bolaji5821 <oyedelebolaji84@gmail.com>
2022-04-07 21:50:17 -0700

    malloc task 1

--- 0x0C-more_malloc_free/1-string_nnconcat.c
+++ 0x0C-more_malloc_free/1-string_nnconcat.c
@@ -1,17 +1,48 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
 /**
- * *string_nconcat: this function concatenates two strings.
- * @s1: contains the first string
- * @s2: contains the secong string
- * @n: number of character to be extracted 
- * Return: returns a pointer.
+ * string_nconcat - a function that allocate memory to a
+ * pointer and concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * @n: number of characters to extract from second string
+ * Return: address of allocated memory
  */
-char *string_nconcat(char *s1, char *s2, unsigned int n);
-	char *count_string
-	unsigned int i;
-	unsigned int j;
-	unsigned int k;
-if 
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *count_string;
+	unsigned int i, j, k;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	for (i = 0; *(s1 + i) != '\0'; i++)
+		;
+	for (j = 0; *(s2 + j) != '\0'; j++)
+		;
+
+	if (n > j)
+		n = j;
+
+	count_string = malloc(((i + n) + 1) * sizeof(char));
+	if (count_string == NULL)
+		return (NULL);
+
+	for (k = 0; k < (i + n); k++)
+	{
+		if (k < i)
+		{
+			count_string[k] = s1[k];
+		}
+		else
+		{
+			count_string[k] = s2[k - i];
+		}
+	}
+	count_string[k] = '\0';
+	return (count_string);
+} 
