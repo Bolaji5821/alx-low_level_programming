f3ec6ba3145d3030179130af2cd1796254cd927f
Bolaji5821 <oyedelebolaji84@gmail.com>
2022-04-07 21:31:08 -0700

    malloc task 0

---
+++ 0x0C-more_malloc_free/1-string_nnconcat.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+/**
+ * *string_nconcat: this function concatenates two strings.
+ * @s1: contains the first string
+ * @s2: contains the secong string
+ * @n: number of character to be extracted 
+ * Return: returns a pointer.
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+	char *count_string
+	unsigned int i;
+	unsigned int j;
+	unsigned int k;
+if 
