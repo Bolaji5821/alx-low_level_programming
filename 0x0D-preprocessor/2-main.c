f0d4302a19dbbbaa4412d14be7e1c022df00da5e
Bolaji5821 <oyedelebolaji84@gmail.com>
2022-04-11 18:58:28 -0700

    __FILE__

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - this function prints the name of the current file
+ *
+ * Return: 0 after succesful run
+ */
+int main(void)
+{
+	printf("%s", __FILE__);
+
+	return (0);
+}
